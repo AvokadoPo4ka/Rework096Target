524c1300dc7475f3741f2346e0f928e3f84bd8d4
AvokadoPo4ka <88880953+AvokadoPo4ka@users.noreply.github.com>
2023-04-29 21:25:22 +0700

    Add files via upload

---
+++ Rework096Target/Config.cs
@@ -0,0 +1,23 @@
+﻿using Exiled;
+using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.ComponentModel;
+
+namespace AddingTarget
+{
+    public class Config : IConfig
+    {
+        [Description("Should plugin work?")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Debug?")]
+        public bool Debug { get; set; } = false;
+        [Description("Broadcast to player, who became SCP096's target")]
+        public string AddingTargetBroadcast { get; set; } = "ВЫ ЦЕЛЬ SCP096";
+        
+    }
+}
---
+++ Rework096Target/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using Exiled;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Scp096;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingTarget
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Prefix => "096Target";
+        public override string Name => "AddingSCP096TargetWarn";
+        public override string Author => "RABB1T#3072";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(6, 1, 0);
+
+        public static Plugin plugin;
+
+        public override void OnEnabled()
+        {
+            Log.Debug("I am ready!");
+            Exiled.Events.Handlers.Scp096.AddingTarget += OnAddingTarget;
+            base.OnEnabled();
+        }
+
+        private void OnAddingTarget(AddingTargetEventArgs ev)
+        {
+            ev.Player.ShowHint(Config.AddingTargetBroadcast, 5f);
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Scp096.AddingTarget -= OnAddingTarget;
+            base.OnDisabled();
+        }
+    }
+}
